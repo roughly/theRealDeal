59a0a3f99512ea9f2ff4291f03a47ec8268f3135
roughly <roughlyjeffrey@gmail.com>
2013-03-13 21:38:12 -0500

    stuff from eric javascript goodness
    :)

---
+++ javascript/greet.js
@@ -0,0 +1,5 @@
+<script language="javascript" src="greet.js">
+</script>
+<script language="javascript">
+greet("Sam");
+</script>
---
+++ javascript/hiThere.js
@@ -0,0 +1,9 @@
+
+<html>
+var myString = "Fluffy is a pretty cat.";
+
+<script>
+
+document.write("The time is: " + new Date());
+
+</script>
